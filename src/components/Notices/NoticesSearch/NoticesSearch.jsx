290aa35e6812a2604348786dba0a7401050423fc
Andrii Zorba <37942049+Zorbik@users.noreply.github.com>
2023-02-19 17:11:21 +0200

    ddd

--- src/components/Notices/NoticesSearch/NoticesSearch.jsx
+++ src/components/Notices/NoticesSearch/NoticesSearch.jsx
@@ -1,27 +1,40 @@
 import React, { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import PropTypes from 'prop-types';
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 
 export const NoticesSearch = ({ onSubmit }) => {
   const [inputValue, setInputValue] = useState();
+
   const onInputChange = e => {
     const { value } = e.target;
-    setInputValue(value);
+    setInputValue(() => value);
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
+    <InputGroup
+      as="form"
+      maxW={{ base: '280px', lg: '608px' }}
+      display={'flex'}
+      alignItems={'center'}
+      onSubmit={onSubmit}
+    >
+      <Input
+        variant={'search'}
         value={inputValue}
         onChange={onInputChange}
         type="text"
         title="Input part of notice title"
         placeholder="Search"
       />
-      <button type="submit">
-        <AiOutlineSearch size="24" />
-      </button>
-    </form>
+      <InputRightElement
+        height={{ base: '40px', lg: '44px' }}
+        my={{ base: '28px', lg: '40px' }}
+        type="submit"
+      >
+        <AiOutlineSearch size={'24'} />
+      </InputRightElement>
+    </InputGroup>
   );
 };
 
