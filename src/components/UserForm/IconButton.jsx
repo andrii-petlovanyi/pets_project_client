34feb8c4e98b9325d9ccb60244c7fb9b465e0290
Iuliia Sokolovska <uu.sokil@gmail.com>
2023-02-19 19:05:21 +0100

    change input

---
+++ src/components/UserForm/IconButton.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { IconButton } from '@chakra-ui/react';
+import { RiPencilFill } from 'react-icons/ri';
+import { GoCheck } from 'react-icons/go';
+import PropTypes from 'prop-types';
+
+export const ButtonUserForm = ({ handleClick, name, flag }) => {
+  return (
+    // {
+    //   name: true,
+    //   email: false,
+    //   birthday: true,
+    //   phone: true,
+    //   city: true,
+    // }
+
+    //name=phone
+    <>
+      <IconButton
+        onClick={flag[name] ? () => handleClick(name) : () => handleClick()}
+        width="32px"
+        height="32px"
+        ml="24px"
+        background="mainColor"
+        backdropFilter="blur(2px)"
+        borderRadius="50px"
+        border="none"
+        icon={
+          flag[name] ? (
+            <RiPencilFill
+              color={
+                Object.values(flag).includes(false)
+                  ? 'rgba(17, 17, 17, 0.6)'
+                  : 'rgba(245, 146, 86, 1)'
+              }
+            />
+          ) : (
+            <GoCheck color="rgba(245, 146, 86, 1)" />
+          )
+        }
+      />
+    </>
+  );
+};
+
+ButtonUserForm.propTypes = {
+  handleClick: PropTypes.func,
+  name: PropTypes.string,
+  flag: PropTypes.any,
+};
--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,16 +1,305 @@
-import { Box, Button, Typography } from '@mui/material';
-import React from 'react';
+// import { Box, Button, Typography } from '@mui/material';
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { useForm, Controller } from 'react-hook-form';
+import { Input, InputGroup } from '@chakra-ui/input';
+import { Button, HStack, Image, Box, Text } from '@chakra-ui/react';
+import { MdPhotoCamera } from 'react-icons/md';
+import { IoIosLogOut } from 'react-icons/io';
+import React, { useState } from 'react';
+import { ButtonUserForm } from '../components/UserForm/IconButton';
 
 const UserDashboard = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      birthday: '',
+      phone: '',
+      city: '',
+    },
+  });
+
+  const INITIAL_DISABLED = {
+    name: true,
+    email: true,
+    birthday: true,
+    phone: true,
+    city: true,
+  };
+
+  const [isDisabled, setIsDisabled] = useState(INITIAL_DISABLED);
+
+  const handleEdit = data => {
+    console.log(data);
+
+    setIsDisabled({ ...INITIAL_DISABLED, [data]: false });
+  };
+
+  const onSubmit = data => console.log(data);
+
   return (
     <div>
-      <Button variant="outlineBG">sell</Button>
-      <Button variant="outlineBGActive">sell</Button>
-      <Button variant="outline">Learn more</Button>
-      <Box bgcolor={'custom.'}>
-        <Typography color={'primary'}>Test 1</Typography>
-        <Typography color={'secondary'}>Test 1</Typography>
-      </Box>
+      <FormControl
+        onSubmit={handleSubmit(onSubmit)}
+        display={'flex'}
+        flexDirection={'column'}
+        maxW={'411'}
+        p={'20px 16px'}
+        background={'#FFFFFF'}
+        boxShadow={'7px 4px 14px rgba(49, 21, 4, 0.07)'}
+        borderRadius={'0px 40px 40px 0px'}
+      >
+        <Box
+          display={'flex'}
+          alignItems={'flex-end'}
+          justifyContent={'flex-end'}
+          mb={'32px'}
+        >
+          <Image
+            borderRadius="full"
+            fallbackSrc="https://via.placeholder.com/233"
+          />
+          <HStack>
+            <Button
+              display={'flex'}
+              justifyContent={'flex-start'}
+              alignItems={'center'}
+              padding={'5px'}
+              position="relative"
+              right={'11px'}
+              backgroundColor={'white'}
+              w={'100%'}
+              leftIcon={<MdPhotoCamera color={'#F59256'} w={18} h={18} />}
+            >
+              <Text
+                color={'rgba(17, 17, 17, 0.6)'}
+                fontFamily={'body'}
+                fontWeight={'400'}
+                fontSize={'12px'}
+                lineHeight={'1.8'}
+                letterSpacing={'0.04em'}
+              >
+                Edit photo
+              </Text>
+            </Button>
+          </HStack>
+        </Box>
+        <InputGroup
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          mb={'15'}
+        >
+          <FormLabel name="name" display={'flex'} alignItems={'center'} m={'0'}>
+            <Text mb={'0'} mr={'0'} w={'107px'}>
+              Name:
+            </Text>
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  variant={'form'}
+                  maxW={'216'}
+                  maxH={'32'}
+                  p={'4px 12px'}
+                  disabled={isDisabled.name}
+                />
+              )}
+            />
+            {isDisabled.name ? (
+              <ButtonUserForm
+                handleClick={handleEdit}
+                name={'name'}
+                flag={isDisabled}
+              />
+            ) : (
+              <ButtonUserForm
+                handleClick={handleSubmit(onSubmit)}
+                name={'name'}
+                flag={isDisabled}
+              />
+            )}
+          </FormLabel>
+        </InputGroup>
+        <InputGroup
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          mb={'15'}
+        >
+          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+            <Text mb={'0'} mr={'0'} w={'107px'}>
+              Email:
+            </Text>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  variant={'form'}
+                  maxW={'216'}
+                  maxH={'32'}
+                  p={'4px 12px'}
+                  disabled={isDisabled.email}
+                />
+              )}
+            />
+            {isDisabled.email ? (
+              <ButtonUserForm
+                handleClick={handleEdit}
+                name={'email'}
+                flag={isDisabled}
+              />
+            ) : (
+              <ButtonUserForm
+                handleClick={handleSubmit(onSubmit)}
+                name={'email'}
+                flag={isDisabled}
+              />
+            )}
+          </FormLabel>
+        </InputGroup>
+        <InputGroup
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          mb={'15'}
+        >
+          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+            <Text mb={'0'} mr={'0'} w={'107px'}>
+              Birthday:
+            </Text>
+            <Controller
+              name="birthday"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  variant={'form'}
+                  maxW={'216'}
+                  maxH={'32'}
+                  p={'4px 12px'}
+                  disabled={isDisabled.birthday}
+                />
+              )}
+            />
+            {isDisabled.birthday ? (
+              <ButtonUserForm
+                handleClick={handleEdit}
+                name={'birthday'}
+                flag={isDisabled}
+              />
+            ) : (
+              <ButtonUserForm
+                handleClick={handleSubmit(onSubmit)}
+                name={'birthday'}
+                flag={isDisabled}
+              />
+            )}
+          </FormLabel>
+        </InputGroup>
+        <InputGroup
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          mb={'15'}
+        >
+          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+            <Text mb={'0'} mr={'0'} w={'107px'}>
+              Phone:
+            </Text>
+            <Controller
+              name="phone"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  variant={'form'}
+                  maxW={'216'}
+                  maxH={'32'}
+                  p={'4px 12px'}
+                  disabled={isDisabled.phone}
+                />
+              )}
+            />
+            {isDisabled.phone ? (
+              <ButtonUserForm
+                handleClick={handleEdit}
+                name={'phone'}
+                flag={isDisabled}
+              />
+            ) : (
+              <ButtonUserForm
+                handleClick={handleSubmit(onSubmit)}
+                name={'phone'}
+                flag={isDisabled}
+              />
+            )}
+          </FormLabel>
+        </InputGroup>
+        <InputGroup
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          mb={'15'}
+        >
+          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+            <Text mb={'0'} mr={'0'} w={'107px'}>
+              City:
+            </Text>
+            <Controller
+              name="city"
+              control={control}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  variant={'form'}
+                  maxW={'216'}
+                  maxH={'32'}
+                  p={'4px 12px'}
+                  disabled={isDisabled.city}
+                />
+              )}
+            />
+            {isDisabled.city ? (
+              <ButtonUserForm
+                handleClick={handleEdit}
+                name={'city'}
+                flag={isDisabled}
+              />
+            ) : (
+              <ButtonUserForm
+                handleClick={handleSubmit(onSubmit)}
+                name={'city'}
+                flag={isDisabled}
+              />
+            )}
+          </FormLabel>
+        </InputGroup>
+        <HStack>
+          <Button
+            mb={'18px'}
+            backgroundColor={'white'}
+            display="flex"
+            alignItems="center"
+            letterSpacing="0.04em"
+            leftIcon={<IoIosLogOut color={'#F59256'} w={18} h={18} />}
+          >
+            <Text
+              color={'rgba(17, 17, 17, 0.6)'}
+              fontFamily={'body'}
+              fontWeight="500"
+              fontSize="16px"
+              lineHeight="1.4"
+            >
+              Log Out
+            </Text>
+          </Button>
+        </HStack>
+      </FormControl>
     </div>
   );
 };
