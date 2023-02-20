058d9a52982cb349a47fcdbc6ba9775aa350e0f7
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2023-02-20 08:26:29 +0200

    incoming change

--- src/pages/OurFriends.jsx
+++ src/pages/OurFriends.jsx
@@ -1,6 +1,15 @@
 import {
   Flex,
-  Heading
+  Heading,
+  Image,
+  Text,
+  CardHeader,
+  Box,
+  Link,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from '@chakra-ui/react';
 import React from 'react';
 import { FriendsCard } from '../components/OurFriends/FriendsCard';
@@ -25,7 +34,86 @@ const OurFriends = () => {
         {!isLoading ? (
           friends.length > 0 &&
           friends.map(f => (
-            <FriendsCard key={f._id} friend={f} />
+            <Card
+              w={{
+                base: '280px',
+                lg: '336px',
+                xl: '395px',
+              }}
+              h={{
+                base: '192px',
+                lg: '246px',
+                xl: '287px',
+              }}
+              key={f._id}
+            >
+              <CardHeader
+                align={'center'}
+                padding={{ base: '12px', lg: '14px', xl: '16px' }}
+              >
+                <Link
+                  color={'mainOrange'}
+                  fontSize={{ base: '12px', lg: '18px', xl: '20px' }}
+                  fontWeight={'700'}
+                  lineHeight={{ base: '14px', lg: '22px', xl: '27px' }}
+                  href={f.url}
+                  target={'_blank'}
+                >
+                  {f.title}
+                </Link>
+              </CardHeader>
+              <Flex>
+                <Box
+                  w={{
+                    base: '110px',
+                    lg: '120px',
+                    xl: '158px',
+                  }}
+                  h={{
+                    base: '78px',
+                    lg: '85px',
+                    xl: '112px',
+                  }}
+                  ml={'4px'}
+                >
+                  <Image src={f.imageUrl} alt="Partner's image" />
+                </Box>
+
+                <CardBody
+                  fontSize={{ base: '12px', lg: '14px', xl: '16px' }}
+                  lineHeight={{ base: '14px', lg: '19px', xl: '22px' }}
+                  pl={{ base: '10px', lg: '16px' }}
+                  pb={{ base: '8px', lg: '16px' }}
+                  pr={{ base: '10px', lg: '16px' }}
+                  pt={0}
+                >
+                  <Box>
+                    <Text>Time:</Text>
+                    <Menu>
+                      <MenuButton>09:00 - 18:00</MenuButton>
+                      <MenuList >
+                        <MenuItem >
+                          <Text>Mn:</Text>
+                          <Text>09:00-18:00</Text>
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </Box>
+                  <Box mt={'4px'}>
+                    <Text>Address:</Text>
+                    <Text>{f.address || '-------------------'}</Text>
+                  </Box>
+                  <Box mt={'4px'}>
+                    <Text>Email:</Text>
+                    <Text> {f.email || '-------------------'} </Text>
+                  </Box>
+                  <Box mt={'4px'}>
+                    <Text>Phone:</Text>
+                    <Text>{f.phone || '-------------------'}</Text>
+                  </Box>
+                </CardBody>
+              </Flex>
+            </Card>
           ))
         ) : (
           <>Loading...</>
