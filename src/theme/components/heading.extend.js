aa80bc6ad676daa81fde9930787a63b6ee011a05
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-20 20:11:32 +0200

    added(Loaders): added folder for loaders

--- src/theme/components/heading.extend.js
+++ src/theme/components/heading.extend.js
@@ -9,6 +9,15 @@ const main = defineStyle({
   fontSize: () => ({ base: '24px', lg: '48px' }),
 });
 
+const modalTitle = defineStyle({
+  fontFamily: 'Manrope',
+  fontWeight: '600',
+  fontSize: () => ({ base: '24px', lg: '36px' }),
+  lineHeight: () => ({ base: '33px', lg: '49px' }),
+
+  color: 'black',
+});
+
 export const headingTheme = defineStyleConfig({
-  variants: { main },
+  variants: { main, modalTitle },
 });
