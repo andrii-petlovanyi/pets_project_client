4c378541bcce66620c3489928c3e61cead19590d
UsernameViktoriia <tori-boss@ukr.net>
2023-02-20 23:00:19 +0200

    work

--- src/components/UserPets/PetsItem.jsx
+++ src/components/UserPets/PetsItem.jsx
@@ -1,48 +1,97 @@
-import React from 'react';
-import {
-  Card,
-  CardBody,
-  Text,
-  Image,
-  Stack,
-  IconButton,
-} from '@chakra-ui/react';
-import { MdOutlineDeleteOutline } from 'react-icons/md';
+// import React from 'react';
+// import {
+//   Card,
+//   CardBody,
+//   Text,
+//   Image,
+//   Stack,
+//   IconButton,
+// } from '@chakra-ui/react';
+// import { MdOutlineDeleteOutline } from 'react-icons/md';
+// import { useDeleteMyPetsMutation } from '../../redux/user/userApiSlice';
+// import { useDispatch } from 'react-redux';
 
-export const UserPetsItem = ({ name, birth, breed, avatarURL, comment }) => {
-  return (
-    <Card
-      direction={{ base: 'column', lg: 'row' }}
-      padding={'20px'}
-      borderRadius={{ base: '20px', lg: '40px' }}
-      boxShadow={'secondShadow'}
-    >
-      <Image
-        borderRadius={{ lg: '40px', base: '20px' }}
-        height={{ lg: '161px', base: '240px' }}
-        width={{ lg: '161px', base: '240px' }}
-        mx="auto"
-        objectFit={'cover'}
-        src={avatarURL}
-        alt={breed}
-      />
-      <CardBody>
-        <Stack>
-          <Text>NAME: {name} </Text>
-          <Text>Date of birth: {birth} </Text>
-          <Text>Breed: {breed} </Text>
-          <Text>{comment}</Text>
-        </Stack>
-        <IconButton
-          variant={'secondIB'}
-          position="absolute"
-          top={{ lg: '20px', base: '276px' }}
-          right="20px"
-          color={'labelColor'}
-          icon={<MdOutlineDeleteOutline size={'24px'} />}
-        />
-      </CardBody>
-    </Card>
-  );
-};
-export default UserPetsItem;
+// export const UserPetsItem = ({ pet = {} }) => {
+//   const { name, birth, breed, avatarURL, comment } = pet;
+//   const onDeletePet = petId => {
+//     dispatch(useDeleteMyPetsMutation(petId));
+//   };
+//   return (
+//     <Card
+//       direction={{ base: 'column', lg: 'row' }}
+//       pt={'16px'}
+//       padding={'20px'}
+//       borderRadius={{ base: '20px', lg: '40px' }}
+//       boxShadow={'secondShadow'}
+//     >
+//       <Image
+//         borderRadius={{ base: '20px', lg: '40px' }}
+//         height={{ base: '240px', lg: '161px' }}
+//         width={{ base: '240px', lg: '161px' }}
+//         mx="auto"
+//         objectFit={'cover'}
+//         src={avatarURL}
+//         alt={breed}
+//       />
+//       <CardBody p={0} ml={{ lg: '32px' }} mt={{ base: '20px', lg: '0' }}>
+//         <Stack>
+//           <Text
+//             color={{ base: 'black', lg: 'textColor' }}
+//             fontSize={{ base: '14px', lg: '16px' }}
+//             lineHeight={{ base: '19px', lg: '22px' }}
+//             letterSpacing={'0.04em'}
+//           >
+//             NAME: {name}{' '}
+//           </Text>
+//           <Text
+//             color={{ base: 'black', lg: 'textColor' }}
+//             fontSize={{ base: '14px', lg: '16px' }}
+//             lineHeight={{ base: '19px', lg: '22px' }}
+//             letterSpacing={'0.04em'}
+//           >
+//             Date of birth: {birth}{' '}
+//           </Text>
+//           <Text
+//             color={{ base: 'black', lg: 'textColor' }}
+//             fontSize={{ base: '14px', lg: '16px' }}
+//             lineHeight={{ base: '19px', lg: '22px' }}
+//             letterSpacing={'0.04em'}
+//           >
+//             Breed: {breed}{' '}
+//           </Text>
+//           <Text
+//             color={{ base: 'black', lg: 'textColor' }}
+//             fontSize={{ base: '14px', lg: '16px' }}
+//             lineHeight={{ base: '19px', lg: '22px' }}
+//             letterSpacing={'0.04em'}
+//           >
+//             {comment}
+//           </Text>
+//         </Stack>
+//         <IconButton
+//           onClick={() => {
+//             onDeletePet({ _id?????????????????????????????????????? });
+//           }}
+//           variant={{ lg: 'secondIB' }}
+//           position="absolute"
+//           top={{ base: '272px', lg: '20px' }}
+//           right={{ base: '12px', lg: '20px' }}
+//           color={'labelColor'}
+//           icon={<MdOutlineDeleteOutline size={24} />}
+//         />
+//       </CardBody>
+//     </Card>
+//   );
+// };
+
+// UserPetsItem.propTypes = {
+//   pet: PropTypes.shape({
+//     name: PropTypes.string,
+//     birth: PropTypes.string,
+//     breed: PropTypes.string,
+//     avatarURL: PropTypes.string,
+//     comment: PropTypes.string,
+//   }),
+// };
+
+// export default UserPetsItem;
--- src/components/UserPets/PetsList.jsx
+++ src/components/UserPets/PetsList.jsx
@@ -10,21 +10,28 @@ import {
 import { MdOutlineDeleteOutline } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 import userSelectors from '../../redux/user/user-selectors';
+// import UserPetsItem from './PetsItem';
 
 export const UserPetsList = () => {
   const { pets } = useSelector(userSelectors.user);
   console.log(pets);
+
   return (
+    // pets.length > 0 &&
+    // pets.map(pet => <UserPetsItem key={pet.avatarURL} pet={pet} />)
+
+    // НИЖЧЕ КОД ВИДАЛИТИ, А ЗАКОМЕНТОВАНИЙ РОЗКОМЕНТУВАТЬ
     <Card
       direction={{ base: 'column', lg: 'row' }}
+      pt={'16px'}
       padding={'20px'}
       borderRadius={{ base: '20px', lg: '40px' }}
       boxShadow={'secondShadow'}
     >
       <Image
-        borderRadius={{ lg: '40px', base: '20px' }}
-        height={{ lg: '161px', base: '240px' }}
-        width={{ lg: '161px', base: '240px' }}
+        borderRadius={{ base: '20px', lg: '40px' }}
+        height={{ base: '240px', lg: '161px' }}
+        width={{ base: '240px', lg: '161px' }}
         mx="auto"
         objectFit={'cover'}
         src={
@@ -32,24 +39,50 @@ export const UserPetsList = () => {
         }
         alt="Cat"
       />
-      <CardBody>
-        <Stack>
-          <Text>NAME: Jack </Text>
-          <Text>Date of birth: 22.04.2018 </Text>
-          <Text>Breed: Persian cat </Text>
-          <Text>
+      <CardBody p={0} ml={{ lg: '32px' }} mt={{ base: '20px', lg: '0' }}>
+        <Stack spacing={'12px'}>
+          <Text
+            color={{ base: 'black', lg: 'textColor' }}
+            fontSize={{ base: '14px', lg: '16px' }}
+            lineHeight={{ base: '19px', lg: '22px' }}
+            letterSpacing={'0.04em'}
+          >
+            NAME: Jack{' '}
+          </Text>
+          <Text
+            color={{ base: 'black', lg: 'textColor' }}
+            fontSize={{ base: '14px', lg: '16px' }}
+            lineHeight={{ base: '19px', lg: '22px' }}
+            letterSpacing={'0.04em'}
+          >
+            Date of birth: 22.04.2018{' '}
+          </Text>
+          <Text
+            color={{ base: 'black', lg: 'textColor' }}
+            fontSize={{ base: '14px', lg: '16px' }}
+            lineHeight={{ base: '19px', lg: '22px' }}
+            letterSpacing={'0.04em'}
+          >
+            Breed: Persian cat{' '}
+          </Text>
+          <Text
+            color={{ base: 'black', lg: 'textColor' }}
+            fontSize={{ base: '14px', lg: '16px' }}
+            lineHeight={{ base: '19px', lg: '22px' }}
+            letterSpacing={'0.04em'}
+          >
             Comments: Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor
             sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem
             ipsum dolor sit amet, consectetur{' '}
           </Text>
         </Stack>
         <IconButton
-          variant={'secondIB'}
+          variant={{ lg: 'secondIB' }}
           position="absolute"
-          top={{ lg: '20px', base: '276px' }}
-          right="20px"
+          top={{ base: '272px', lg: '20px' }}
+          right={{ base: '12px', lg: '20px' }}
           color={'labelColor'}
-          icon={<MdOutlineDeleteOutline size={'24px'} />}
+          icon={<MdOutlineDeleteOutline size={24} />}
         />
       </CardBody>
     </Card>
