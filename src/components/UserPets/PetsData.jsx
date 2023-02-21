4203f8ab075c97c277942ba04607db1742f4b438
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-21 20:36:30 +0200

    added(PublicRoute): added Home in public route

--- src/components/UserPets/PetsData.jsx
+++ src/components/UserPets/PetsData.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Flex, Heading, } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import UserPetsList from './PetsList';
 import AddPets from './AddPets';
 
 export const UserPetsData = () => {
   return (
     <Flex flexDirection={'column'} width={'100%'}>
-      <Flex justifyContent={'space-between'} >
+      <Flex justifyContent={'space-between'} mb={{ base: '32px', lg: '24px' }}>
         <Heading mb={'20px'}>My pets:</Heading>
         <AddPets />
       </Flex>
