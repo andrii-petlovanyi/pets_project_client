5be4787cddbd090617da45b57389d8a3362641b0
Iuliia Sokolovska <uu.sokil@gmail.com>
2023-02-21 18:51:38 +0100

    avatar, style

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -8,13 +8,18 @@ import { FormControl, FormLabel } from '@chakra-ui/form-control';
 import { Input, InputGroup } from '@chakra-ui/input';
 import {
   Box,
+  Flex,
+  Heading,
   // Button, HStack, Image, Box, Icon,
   Text,
 } from '@chakra-ui/react';
-import { calendarFunc } from '../UserForm/Calendar';
+import { calendarFunc } from './Calendar/Calendar';
 import { ButtonUserForm } from '../UserForm/IconButton';
 import { userFormSchema } from '../../services/validation';
-import Avatar from './Avatar';
+// import Avatar from './Avatar';
+import './Calendar/Calendar.styled.css';
+
+import UserAvatar from './Avatar';
 
 const UserForm = () => {
   const {
@@ -58,241 +63,253 @@ const UserForm = () => {
         <Input value={'data'} variant={open ? 'userInfoActive' : 'userInfoDisabled'} />
         <IconButton variant={'style'} color={!oneInput ?? 'grey'} icon={open ? <MdPlace /> : <Md10K />} />
       </FormControl> */}
-      <FormControl
-        onSubmit={handleSubmit(onSubmit)}
-        display={'flex'}
-        flexDirection={'column'}
-        maxW={'411'}
-        p={'20px 16px 67px 16px'}
-        background={'white'}
-        boxShadow={'7px 4px 14px rgba(49, 21, 4, 0.07)'}
-        borderRadius={'0px 40px 40px 0px'}
-      >
-        <Avatar />
-        <InputGroup
+      <Flex flexDirection={'column'} width={'100%'}>
+        <Heading mb={'24px'}>My information:</Heading>
+        <FormControl
+          onSubmit={handleSubmit(onSubmit)}
           display={'flex'}
           flexDirection={'column'}
-          alignItems={'center'}
-          justifyContent={'space-around'}
-          mb={'15'}
+          maxW={'411'}
+          p={'20px 16px 67px 16px'}
+          background={'white'}
+          boxShadow={'7px 4px 14px rgba(49, 21, 4, 0.07)'}
+          borderRadius={'0px 40px 40px 0px'}
         >
-          <FormLabel name="name" display={'flex'} alignItems={'center'} m={'0'}>
-            <Text variant={'textLabelUserForm'}>Name:</Text>
-            <Controller
-              name="name"
-              control={control}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  borderRadius={'40px'}
-                  variant={
-                    isDisabled.name ? 'userInfoDisabled' : 'userInfoActive'
-                  }
-                  disabled={isDisabled.name}
-                />
-              )}
-            />
-
-            {isDisabled.name ? (
-              <ButtonUserForm
-                handleClick={handleEdit}
-                name={'name'}
-                flag={isDisabled}
-              />
-            ) : (
-              <ButtonUserForm
-                handleClick={handleSubmit(onSubmit)}
-                name={'name'}
-                flag={isDisabled}
-              />
-            )}
-          </FormLabel>
-          <Text color={'red.500'} fontSize={'12px'}>
-            {errors.name?.message}
-          </Text>
-        </InputGroup>
-        <InputGroup
-          display={'flex'}
-          flexDirection="column"
-          alignItems={'center'}
-          justifyContent={'space-around'}
-          mb={'15'}
-        >
-          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
-            <Text variant={'textLabelUserForm'}>Email:</Text>
-            <Controller
-              name="email"
-              control={control}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  borderRadius={'40px'}
-                  variant={
-                    isDisabled.email ? 'userInfoDisabled' : 'userInfoActive'
-                  }
-                  disabled={isDisabled.email}
-                />
-              )}
-            />
-            {isDisabled.email ? (
-              <ButtonUserForm
-                handleClick={handleEdit}
-                name={'email'}
-                flag={isDisabled}
-              />
-            ) : (
-              <ButtonUserForm
-                handleClick={handleSubmit(onSubmit)}
-                name={'email'}
-                flag={isDisabled}
-              />
-            )}
-          </FormLabel>
-          <Text color={'red.500'} fontSize={'12px'}>
-            {errors.email?.message}
-          </Text>
-        </InputGroup>
-        <InputGroup
-          display={'flex'}
-          flexDirection="column"
-          alignItems={'center'}
-          justifyContent={'space-around'}
-          mb={'15'}
-        >
-          <FormLabel
-            htmlFor={'birthday'}
+          <UserAvatar />
+          <InputGroup
             display={'flex'}
+            flexDirection={'column'}
             alignItems={'center'}
-            m={'0'}
+            justifyContent={'space-around'}
+            mb={'15'}
           >
-            <Text variant={'textLabelUserForm'}>Birthday:</Text>
-            <Controller
-              name="birthday"
-              control={control}
-              render={({ field }) => (
-                // <Calendar />
-                <Box
-                  style={{ width: '216px', height: '32px' }}
-                // variant={
-                //   isDisabled.birthday ? 'userInfoDisabled' : 'userInfoActive'
-                // }
-                >
-                  <DatePicker
-                    renderCustomHeader={calendarFunc}
-                    disabled={isDisabled.birthday}
-                    onChange={date => field.onChange(date)}
-                    selected={field.value}
-                    dateFormat="dd.MM.yyyy"
-                    maxDate={Date.now()}
-                  //   borderRadius={'40px'}
-                  //   variant={
-                  //     isDisabled.birthday ? 'userInfoDisabled' : 'userInfoActive'
-                  //   }
+            <FormLabel
+              name="name"
+              display={'flex'}
+              alignItems={'center'}
+              m={'0'}
+            >
+              <Text variant={'textLabelUserForm'}>Name:</Text>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    borderRadius={'40px'}
+                    variant={
+                      isDisabled.name ? 'userInfoDisabled' : 'userInfoActive'
+                    }
+                    disabled={isDisabled.name}
                   />
-                </Box>
-              )}
-            />
+                )}
+              />
 
-            {isDisabled.birthday ? (
-              <ButtonUserForm
-                handleClick={handleEdit}
-                name={'birthday'}
-                flag={isDisabled}
-              />
-            ) : (
-              <ButtonUserForm
-                handleClick={handleSubmit(onSubmit)}
-                name={'birthday'}
-                flag={isDisabled}
-              />
-            )}
-          </FormLabel>
-          <Text color={'red.500'} fontSize={'12px'}>
-            {errors.birthday?.message}
-          </Text>
-        </InputGroup>
-        <InputGroup
-          display={'flex'}
-          flexDirection="column"
-          alignItems={'center'}
-          justifyContent={'space-around'}
-          mb={'15'}
-        >
-          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
-            <Text variant={'textLabelUserForm'}>Phone:</Text>
-            <Controller
-              name="phone"
-              control={control}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  borderRadius={'40px'}
-                  variant={
-                    isDisabled.phone ? 'userInfoDisabled' : 'userInfoActive'
-                  }
-                  disabled={isDisabled.phone}
+              {isDisabled.name ? (
+                <ButtonUserForm
+                  handleClick={handleEdit}
+                  name={'name'}
+                  flag={isDisabled}
+                />
+              ) : (
+                <ButtonUserForm
+                  handleClick={handleSubmit(onSubmit)}
+                  name={'name'}
+                  flag={isDisabled}
                 />
               )}
-            />
-            {isDisabled.phone ? (
-              <ButtonUserForm
-                handleClick={handleEdit}
-                name={'phone'}
-                flag={isDisabled}
+            </FormLabel>
+            <Text color={'red.500'} fontSize={'12px'}>
+              {errors.name?.message}
+            </Text>
+          </InputGroup>
+
+          <InputGroup
+            display={'flex'}
+            flexDirection="column"
+            alignItems={'center'}
+            justifyContent={'space-around'}
+            mb={'15'}
+          >
+            <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+              <Text variant={'textLabelUserForm'}>Email:</Text>
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    borderRadius={'40px'}
+                    variant={
+                      isDisabled.email ? 'userInfoDisabled' : 'userInfoActive'
+                    }
+                    disabled={isDisabled.email}
+                  />
+                )}
               />
-            ) : (
-              <ButtonUserForm
-                handleClick={handleSubmit(onSubmit)}
-                name={'phone'}
-                flag={isDisabled}
-              />
-            )}
-          </FormLabel>
-          <Text color={'red.500'} fontSize={'12px'}>
-            {errors.phone?.message}
-          </Text>
-        </InputGroup>
-        <InputGroup
-          display={'flex'}
-          flexDirection="column"
-          alignItems={'center'}
-          justifyContent={'space-around'}
-        >
-          <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
-            <Text variant={'textLabelUserForm'}>City:</Text>
-            <Controller
-              name="city"
-              control={control}
-              render={({ field }) => (
-                <Input
-                  {...field}
-                  borderRadius={'40px'}
-                  variant={
-                    isDisabled.city ? 'userInfoDisabled' : 'userInfoActive'
-                  }
-                  disabled={isDisabled.city}
+              {isDisabled.email ? (
+                <ButtonUserForm
+                  handleClick={handleEdit}
+                  name={'email'}
+                  flag={isDisabled}
+                />
+              ) : (
+                <ButtonUserForm
+                  handleClick={handleSubmit(onSubmit)}
+                  name={'email'}
+                  flag={isDisabled}
                 />
               )}
-            />
-            {isDisabled.city ? (
-              <ButtonUserForm
-                handleClick={handleEdit}
-                name={'city'}
-                flag={isDisabled}
+            </FormLabel>
+            <Text color={'red.500'} fontSize={'12px'}>
+              {errors.email?.message}
+            </Text>
+          </InputGroup>
+
+          <InputGroup
+            display={'flex'}
+            flexDirection="column"
+            alignItems={'center'}
+            justifyContent={'space-around'}
+            mb={'15'}
+          >
+            <FormLabel
+              htmlFor={'birthday'}
+              display={'flex'}
+              alignItems={'center'}
+              m={'0'}
+            >
+              <Text variant={'textLabelUserForm'}>Birthday:</Text>
+              <Controller
+                name="birthday"
+                control={control}
+                render={({ field }) => (
+                  // <Calendar />
+                  <Box
+                    style={{ width: '216px', height: '32px' }}
+                    // variant={
+                    //   isDisabled.birthday ? 'userInfoDisabled' : 'userInfoActive'
+                    // }
+                  >
+                    <DatePicker
+                      renderCustomHeader={calendarFunc}
+                      disabled={isDisabled.birthday}
+                      onChange={date => field.onChange(date)}
+                      selected={field.value}
+                      dateFormat="dd.MM.yyyy"
+                      maxDate={Date.now()}
+                      wrapperClassName="datePicker"
+                      //   borderRadius={'40px'}
+                      //   variant={
+                      //     isDisabled.birthday ? 'userInfoDisabled' : 'userInfoActive'
+                      //   }
+                    />
+                  </Box>
+                )}
               />
-            ) : (
-              <ButtonUserForm
-                handleClick={handleSubmit(onSubmit)}
-                name={'city'}
-                flag={isDisabled}
+              {isDisabled.birthday ? (
+                <ButtonUserForm
+                  handleClick={handleEdit}
+                  name={'birthday'}
+                  flag={isDisabled}
+                />
+              ) : (
+                <ButtonUserForm
+                  handleClick={handleSubmit(onSubmit)}
+                  name={'birthday'}
+                  flag={isDisabled}
+                />
+              )}
+            </FormLabel>
+            <Text color={'red.500'} fontSize={'12px'}>
+              {errors.birthday?.message}
+            </Text>
+          </InputGroup>
+
+          <InputGroup
+            display={'flex'}
+            flexDirection="column"
+            alignItems={'center'}
+            justifyContent={'space-around'}
+            mb={'15'}
+          >
+            <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+              <Text variant={'textLabelUserForm'}>Phone:</Text>
+              <Controller
+                name="phone"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    borderRadius={'40px'}
+                    variant={
+                      isDisabled.phone ? 'userInfoDisabled' : 'userInfoActive'
+                    }
+                    disabled={isDisabled.phone}
+                  />
+                )}
               />
-            )}
-          </FormLabel>
-          <Text color={'red.500'} fontSize={'12px'}>
-            {errors.city?.message}
-          </Text>
-        </InputGroup>
-      </FormControl>
+              {isDisabled.phone ? (
+                <ButtonUserForm
+                  handleClick={handleEdit}
+                  name={'phone'}
+                  flag={isDisabled}
+                />
+              ) : (
+                <ButtonUserForm
+                  handleClick={handleSubmit(onSubmit)}
+                  name={'phone'}
+                  flag={isDisabled}
+                />
+              )}
+            </FormLabel>
+            <Text color={'red.500'} fontSize={'12px'}>
+              {errors.phone?.message}
+            </Text>
+          </InputGroup>
+
+          <InputGroup
+            display={'flex'}
+            flexDirection="column"
+            alignItems={'center'}
+            justifyContent={'space-around'}
+          >
+            <FormLabel display={'flex'} alignItems={'center'} m={'0'}>
+              <Text variant={'textLabelUserForm'}>City:</Text>
+              <Controller
+                name="city"
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    {...field}
+                    borderRadius={'40px'}
+                    variant={
+                      isDisabled.city ? 'userInfoDisabled' : 'userInfoActive'
+                    }
+                    disabled={isDisabled.city}
+                  />
+                )}
+              />
+              {isDisabled.city ? (
+                <ButtonUserForm
+                  handleClick={handleEdit}
+                  name={'city'}
+                  flag={isDisabled}
+                />
+              ) : (
+                <ButtonUserForm
+                  handleClick={handleSubmit(onSubmit)}
+                  name={'city'}
+                  flag={isDisabled}
+                />
+              )}
+            </FormLabel>
+            <Text color={'red.500'} fontSize={'12px'}>
+              {errors.city?.message}
+            </Text>
+          </InputGroup>
+        </FormControl>
+      </Flex>
     </>
   );
 };
