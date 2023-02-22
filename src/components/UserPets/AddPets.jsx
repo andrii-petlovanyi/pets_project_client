69a94a3a25ea91f0a626a13a9b4a914889b4f72f
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-22 22:38:23 +0200

    added(some)

--- src/components/UserPets/AddPets.jsx
+++ src/components/UserPets/AddPets.jsx
@@ -70,7 +70,7 @@ const AddPets = () => {
 
   const [addMyPets, { isLoading }] = useAddMyPetsMutation();
   const dispatch = useDispatch();
-  const { addToast } = Toast();
+  const { addToast } = Toast();gi
 
   const {
     register,
