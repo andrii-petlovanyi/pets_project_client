09b1e3823711f3cab36d4cf12982eef4dee2322b
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-02-22 10:57:01 +0200

    fixed(user): user birth default

--- src/services/dateFormat.js
+++ src/services/dateFormat.js
@@ -4,10 +4,9 @@ export const stringToDate = date => {
 };
 
 export const dateToString = date => {
-  return new Date(date)
-    .toISOString()
-    .split('T')[0]
-    .split('-')
-    .reverse()
-    .join('.');
+  const newDate = new Date(date);
+  let day = newDate.getDate().toString().padStart(2, '0');
+  let month = (newDate.getMonth() + 1).toString().padStart(2, '0');
+  let year = newDate.getFullYear().toString();
+  return `${day}.${month}.${year}`;
 };
